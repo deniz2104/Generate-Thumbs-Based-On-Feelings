2a05de96f8a74784aa2b45d9273a13bb1e4da1bc
deniz2104 <sined2101@gmail.com>
2025-06-03 11:25:36 +0300

    add thresholds for methods and made saving in different folder for fast classification

--- detect_faces_having_motion_with_laplacian_with_kernel.py
+++ detect_faces_having_motion_with_laplacian_with_kernel.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-def is_motion_detected_using_laplacian_with_kernel(image_path,threshold=75):
+def is_motion_detected_using_laplacian_with_kernel(image_path,threshold=80):
     image=cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     kernel=np.array([[-1,2,-1]],dtype=np.float32)
@@ -8,5 +8,4 @@ def is_motion_detected_using_laplacian_with_kernel(image_path,threshold=75):
     dy = cv2.filter2D(gray, cv2.CV_64F, kernel.T)
     modified_lap = np.abs(dx) + np.abs(dy)
     laplacian_var = np.var(modified_lap)
-    print(laplacian_var)
-    ## sa vad aici ce thresholding sa pun
+    return laplacian_var < threshold
--- eliminating_faces_with_motion.py
+++ eliminating_faces_with_motion.py
@@ -1,20 +1,22 @@
 import os
 from ultralytics import YOLO
+from PIL import Image
 import cv2
 from detect_faces_having_motion_with_fft import *
 from detect_faces_having_motion_with_laplacian import *
 from detect_faces_having_motion_with_laplacian_with_kernel import *
-def eliminate_photos_with_motion(image_directory, fft_threshold=140, radius=60,save_directory=None):
+def eliminate_photos_with_motion(image_directory, fft_threshold=140, radius=60, save_directory=None):
     if save_directory and not os.path.exists(save_directory):
         os.makedirs(save_directory)
-    model = YOLO("widerface_yolo/yolo_model/weights/best.pt",verbose=False)
+    model = YOLO("widerface_yolo/yolo_model/weights/best.pt", verbose=False)
     image_files = [f for f in os.listdir(image_directory) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
     
     for image in image_files:
         image_path = os.path.join(image_directory, image)
-        img=cv2.imread(image_path)
-        print(f"\nProcessing: {image_path}")
-        is_motion_detected_using_laplacian_with_kernel(image_path,threshold=75)
-        print(f"\nProcessing with fft: {image_path}")
-        is_image_blurred(image_path, model, fft_threshold=fft_threshold, radius=radius)
-    ##THRESHOLDING 80 sa vedem ce se intamppla
+        img= cv2.imread(image_path)
+        if not (is_motion_detected_using_laplacian(image_path, threshold=80) or
+            is_motion_detected_using_laplacian_with_kernel(image_path, threshold=80) or
+            is_image_blurred(image_path, model, fft_threshold=fft_threshold, radius=radius)):
+            if save_directory:
+                img = Image.open(image_path)
+                img.save(os.path.join(save_directory, image))
